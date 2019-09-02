374cef98b2c4758cf5ad0e10bb8c06279537cc82
Alexander Bedulin <alexander.bedulin@gmail.com>
2019-09-02 14:35:58 +0300

    Android projects renamed, removed unneeded projects

---
+++ Ziggeo.Xamarin.Android/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Com.Ziggeo.Androidsdk;
+
+namespace Ziggeo
+{
+    public partial class Player : IZiggeoPlayer
+    {
+        public Player(IZiggeoApplication application)
+        {
+            this.Application = application;
+            this.Ziggeo = ((ZiggeoApplication) Application).Ziggeo;
+        }
+
+        private IZiggeo Ziggeo;
+        public Dictionary<string, string> AdditionalParameters { get; set; }
+
+        public IZiggeoApplication Application { get; private set; }
+
+        public Task Play(string videoToken)
+        {
+            TaskCompletionSource<int> tcs = new TaskCompletionSource<int>();
+            try
+            {
+             //   Ziggeo.SetExtraArgsForPlayer(AdditionalParameters);
+                Ziggeo.StartPlayer(videoToken);
+            }
+            catch (Exception ex)
+            {
+                tcs.TrySetException(ex);
+            }
+
+            return tcs.Task;
+        }
+    }
+}
